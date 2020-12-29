8973ca3a7cf88075e7f62cdfa7052b59c0d0eb07
Wale-K <olawale.k17@gmail.com>
2020-12-29 01:44:10 +0000

    I added arrow buttons to iterate through the different stages of the introduction. Once you get to the end of the intro it takes you to the recipe page.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,39 +1,60 @@
 import React from "react";
 import styled from "styled-components";
-import { colourPalette, rightArrow } from "../utilities";
+import { colourPalette, rightArrow, leftArrow } from "../utilities";
 
 const Section = styled.div`
   display: ${(props) => props.toggleDisplay};
   flex-direction: column;
-
+  margin: 0 auto;
+  width: 100%;
   svg {
     width: 10vw;
     height: 5vh;
+    align-self: ${(props) => props.align};
+  }
+
+  input {
+    width: 80vw;
+    margin-bottom: 2rem;
+  }
+
+  button {
   }
 `;
 
 const Title = styled.h1`
   text-align: center;
   margin: 0;
-  padding-top: 10vh;
+
   color: ${colourPalette.primary};
+`;
+
+const Arrows = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 const LandingPageContainer = styled.div`
   display: ${(props) => props.toggleDisplay};
+  flex-direction: column;
   background-color: ${colourPalette.background};
   height: 90vh;
   padding: 2rem;
   span {
+    color: blue;
     :hover {
       cursor: pointer;
     }
+  }
+  > div:first-child {
+    margin-top: 10vh;
+    background-color: pink;
   }
 `;
 
 export class LandingPage extends React.Component {
   state = {
-    userName: "",
+    userName: "Wale",
     introDisplay: true,
     middleDisplay: false,
     endDisplay: false,
@@ -87,7 +108,11 @@ export class LandingPage extends React.Component {
       <LandingPageContainer
         toggleDisplay={this.props.displayLandingPage ? "flex" : "none"}
       >
-        <Section toggleDisplay={this.state.introDisplay ? "flex" : "none"}>
+        <div />
+        <Section
+          toggleDisplay={this.state.introDisplay ? "flex" : "none"}
+          align="flex-end"
+        >
           <Title>Recipe Finder</Title>
           <p>Hello friend.</p>
           <p>
@@ -105,15 +130,33 @@ export class LandingPage extends React.Component {
         </Section>
         <Section toggleDisplay={this.state.middleDisplay ? "flex" : "none"}>
           <input
+            placeholder="What's your name?"
             value={this.state.userName}
             onChange={this.handleChangeUserName.bind(this)}
           />
-          <button onClick={this.handleToggleDisplay}>That's my name!</button>
-          <button onClick={this.revertBackAPage}>Back</button>
+          <Arrows>
+            <svg onClick={this.revertBackAPage}>{leftArrow}</svg>
+            <svg onClick={this.handleToggleDisplay}>{rightArrow}</svg>
+          </Arrows>
         </Section>
         <Section toggleDisplay={this.state.endDisplay ? "flex" : "none"}>
           <p>Your name is "{this.state.userName}"? That's a cool name!</p>
-          <button onClick={this.revertBackAPage}>Back</button>
+          <p>
+            Well {this.state.userName}, to use this site all you need to do is
+            enter your ingredients in the input fields on the next page.
+          </p>
+          <p>
+            Once you do that click search and you'll be shown some recipes if
+            there are any matches.
+          </p>
+          <p>
+            If that all makes sense and you're ready to search, you can go to
+            the next page!
+          </p>
+          <Arrows>
+            <svg onClick={this.revertBackAPage}>{leftArrow}</svg>
+            <svg onClick={this.props.skipInstructions}>{rightArrow}</svg>
+          </Arrows>
         </Section>
       </LandingPageContainer>
     );
